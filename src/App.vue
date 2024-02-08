7fa02ae30a57d54ed66812879907117aa97a1b1a
ana.orozco@autonoma.edu.co <ana.orozco@autonoma.edu.co>
2024-02-08 16:31:07 -0500

    Configuration Navbar and Favicoin

--- src/App.vue
+++ src/App.vue
@@ -6,12 +6,22 @@
 
 <!-- HTML ------------------------------------------------------------>
 <template>
-    <div>
-        <navbar />
+    <navbar />
+    
+    <div class="containerHome">
+        
     </div>
 </template>
 
 <!-- STYLES ---------------------------------------------------------->
 <style>
     @import url('https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css');
+
+    .containerHome{        
+        height:     87vh;
+        margin-top: 2px;
+        background-size:  100% 100%;
+        background-image:  url("https://static.vecteezy.com/system/resources/previews/007/460/645/non_2x/food-seasoning-background-round-wooden-cutting-board-spices-herbs-photo.jpg");
+        background-repeat: no-repeat;
+    }
 </style>
--- src/components/navBar/navbar.vue
+++ src/components/navBar/navbar.vue
@@ -2,10 +2,57 @@
 <style  src="./sytles.css"></style>
 
 <template>
-    <div class="containerNarbar">
-        <h1>FILTROS</h1>
-        <h1>ORDENAMIENTOS</h1>
-        <h1>SESION</h1>
-    </div>
-</template>
+    <nav class="navbar navbar-expand-lg navbar-light bg-warning fw-bold">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="Inicio">
+                <img src="../../../FACE.png" alt="" width="50" height="50">
+            </a>                     
+            
+            <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                <ul class="navbar-nav">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="Inicio">Inicio</a>
+                    </li>
+                    
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="Bebidas" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Bebidas
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <li><a class="fw-bold dropdown-item" href="Bebidas0">Todas</a></li>
+                            <li><a class="dropdown-item" href="Bebidas1">Jugos</a></li>
+                            <li><a class="dropdown-item" href="Bebidas2">Batidos</a></li>
+                            <li><a class="dropdown-item" href="Bebidas3">Gaseosas</a></li>
+                        </ul>
+                    </li>
+                    
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="Comidas" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Comidas
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <li><a class="fw-bold dropdown-item" href="Comidas0">Todas</a></li>
+                            <li><a class="dropdown-item" href="Comidas1">Hamburguesas</a></li>
+                            <li><a class="dropdown-item" href="Comidas2">Perros</a></li>
+                            <li><a class="dropdown-item" href="Comidas3">Carnes</a></li>
+                        </ul>
+                    </li>
 
+                    <div class="otherLinks">
+                        <li class="nav-item">
+                            <a class="nav-link" href="Reservas">Reservas</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="MiPedido">Mi pedido</a>
+                        </li>
+
+                        <li class="nav-item">
+                            <a class="nav-link" href="Administrador">Administrador</a>
+                        </li>
+                    </div>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</template>
